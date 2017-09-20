92fd4b267b07ebf75d2bd6c29c7d83159103c364
Jameson Trinker <jameson.trinker@gmail.com>
2017-09-20 13:58:20 -0400

    initial commit, set up new project, set up materialize

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <div className="header-wrapper">
+            <div className="container">
+              <header className="valign-wrapper">
+                <h1>track my stack</h1>
+              </header>
+            </div>
+          </div>
+          <div className="container">
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default App;
